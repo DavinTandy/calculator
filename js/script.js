6c3861b1801024a9d78aabe3abcdc3825fe223e1
DavinTandy <dav.tandy@gmail.com>
2024-11-18 22:44:36 +0700

    Add function of basic math operations (add, subtract, multiply, divide)

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operator(num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "×") {
+        return multiply(num1, num2);
+    } else if (operator === "÷") {
+        return divide(num1, num2);
+    }
+}
