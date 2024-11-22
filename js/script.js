39210158f525e8b71bd6f035ef1e3c3e6193e9db
DavinTandy <dav.tandy@gmail.com>
2024-11-22 07:14:37 +0700

    Display brackets

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const operators = document.getElementsByClassName("operator");
 const percent = document.getElementById("percent");
 const comma = document.getElementById("comma");
 let commaBool = true;
+const bracket = document.getElementById("bracket");
+let openBracket = 0;
 
 clear.addEventListener("click", function() {
     line1.textContent = "0";
@@ -16,6 +18,8 @@ clear.addEventListener("click", function() {
 del.addEventListener("click", function() {
     if (line1.textContent.charAt(line1.textContent.length - 1) === ".") {
         commaBool = true;
+    } else if (line1.textContent.charAt(line1.textContent.length - 1) === "(") {
+        openBracket -= 1;
     }
     line1.textContent = line1.textContent.slice(0, -1);
 })
@@ -34,7 +38,7 @@ for (let i = 0; i < numbers.length; i++) {
 
 for (let i = 0; i < operators.length; i++) {
     operators[i].addEventListener("click", function() {
-        if (line1.textContent === "0") {
+        if (line1.textContent === "0" || line1.textContent.charAt(line1.textContent.length - 1) === "(") {
             return;
         }
         for (let j = 0; j < operators.length; j++) {
@@ -48,7 +52,7 @@ for (let i = 0; i < operators.length; i++) {
 }
 
 percent.addEventListener("click", function() {
-    if (line1.textContent.charAt(line1.textContent.length - 1) === "%") {
+    if (line1.textContent.charAt(line1.textContent.length - 1) === "%" || line1.textContent.charAt(line1.textContent.length - 1) === "(") {
         return;
     }
     for (let i = 0; i < operators.length; i++) {
@@ -61,14 +65,56 @@ percent.addEventListener("click", function() {
 
 comma.addEventListener("click", function() {
     for (let i = 0; i < operators.length; i++) {
-        if (line1.textContent.charAt(line1.textContent.length - 1) === operators[i].textContent) {
+        if (line1.textContent.charAt(line1.textContent.length - 1) === operators[i].textContent || line1.textContent.charAt(line1.textContent.length - 1) === "(") {
             line1.textContent += "0" + comma.textContent;
+            commaBool = false;
+        }
+        if (line1.textContent.charAt(line1.textContent.length - 1) === ")") {
+            line1.textContent += "×0" + comma.textContent;
             commaBool = false;
         }
     }
     if (commaBool === true) {
         line1.textContent += comma.textContent;
         commaBool = false;
+    }
+})
+
+bracket.addEventListener("click", function() {
+    if (openBracket === 0) {
+        if (line1.textContent === "0") {
+            line1.textContent = "(";
+            openBracket += 1;
+            return;
+        } else if (line1.textContent.charAt(line1.textContent.length - 1) === ")") {
+            line1.textContent += "×(";
+            openBracket += 1;
+            return;
+        }
+        for (let i = 0; i < numbers.length; i++) {
+            if (line1.textContent.charAt(line1.textContent.length - 1) === numbers[i].textContent) {
+                line1.textContent += "×(";
+                openBracket += 1;
+                return;
+            }
+        }
+        line1.textContent += "(";
+        openBracket += 1;
+    } else {
+        if (line1.textContent.charAt(line1.textContent.length - 1) === "(") {
+            line1.textContent += "(";
+            openBracket += 1;
+            return;
+        }
+        for (let i = 0; i < operators.length; i++) {
+            if (line1.textContent.charAt(line1.textContent.length - 1) === operators[i].textContent) {
+                line1.textContent += "(";
+                openBracket += 1;
+                return;
+            }
+        }
+        line1.textContent += ")";
+        openBracket -= 1;
     }
 })
 
